96f50d3ae2dd84f4acc5cec30018549e075128a9
PierreG71 <pierreg@allems.co.za>
2021-11-08 21:56:32 +0200

    Add GalleryDetailModel, Midle of Azure storage setup

---
+++ PhotoAlbumShare/Controllers/ImageController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using PhotoAlbumShare.Models;
+
+namespace PhotoAlbumShare.Controllers
+{
+    public class ImageController : Controller
+    {
+        private IConfiguration Config;
+        private string AzureStorageConnectionString { get; }
+
+        public ImageController(IConfiguration config)
+        {
+            Config = config;
+            AzureStorageConnectionString = Config["AzureStorageConnectionString"];
+        }
+
+        public IActionResult Upload()
+        {
+            var model = new UploadImageModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult UploadNewImage()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ PhotoAlbumShare/Models/UploadImageModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace PhotoAlbumShare.Models
+{
+    public class UploadImageModel
+    {
+        public string Title { get; set; }
+        public string Tags { get; set; }
+        public IFormFile ImageUpload { get; set; }
+    }
+}
